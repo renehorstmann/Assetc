ff0588198b598787875eea5106ccfde55c77017b
renehorstmann <r.horsty@gmail.com>
2020-05-25 15:50:50 +0200

    example init

--- example/main.c
+++ example/main.c
@@ -1,26 +1,3 @@
-# Assetc
-Compile asset files or whole directories into a C source file
-
-
-## In active development!
-
-## Compilation of assets
-To compile an asset file or directory, see the examples below:
-```bash
-$ assetc example.txt
-# compiles the content of example.txt into asset.c
-
-$ assetc example.txt -o asset_file_name.c
-# -o changes the key of the compiled file
-
-$ assetc assets_dir
-# will recursivly compile all files, except of hidden ones, into asset.c
-```
-
-
-## Usage of compiled assets
-Compile the generated asset.c file along your C code and use it like so:
-```c
 #include <stdio.h>
 #include <assert.h>
 #include "assetc.h"
@@ -67,7 +44,7 @@ int main() {
     assert(!not_available.data);
 
     // no need to free up asset
-    
+
     // will print:
     //hello size: 11
     //hello data: 0x55ed90a58000
@@ -77,5 +54,3 @@ int main() {
     //array data: 0x55ed90a5801a
     //array content: < 0x00  0xff  0x0f  0xf0 >
 }
-
-```
